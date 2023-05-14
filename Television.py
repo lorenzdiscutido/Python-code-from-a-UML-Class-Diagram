4dbcbe256eaa7de7457afc3a830a11705b043f41
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-14 14:30:31 +0800

    Made a class named TV and a constructor

--- Television.py
+++ Television.py
@@ -1,5 +1,13 @@
 #make a class and name it TV
+class TV:
 #Make a constructor with the television's functions
+    def __init__(self, turnOn, turnOff, getChannel, setChannel, getVolume, setVolume, channelUp, channelDown, volumeUp, VolumeDown):
+        #Initiate the tv on and off of the tv
+        self.turnOn=True
+        self.turnOff=False
+        #Set the volume and channel to 1
+        self.channel=1
+        self.volume=1
 #Create the first object
 #Create the second object
 #Output the two object

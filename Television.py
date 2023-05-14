126485f385c2ea7a40fa2686e43d38aea863fad8
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-14 14:35:02 +0800

    added the function to get the current channel of the TV

--- Television.py
+++ Television.py
@@ -2,12 +2,18 @@
 class TV:
 #Make a constructor with the television's functions
     def __init__(self, turnOn, turnOff, getChannel, setChannel, getVolume, setVolume, channelUp, channelDown, volumeUp, VolumeDown):
+
         #Initiate the tv on and off of the tv
         self.turnOn=True
         self.turnOff=False
+
         #Set the volume and channel to 1
         self.channel=1
         self.volume=1
+
+        #get the channel of the TV
+        def getChannel():
+            return self.channel
 #Create the first object
 #Create the second object
 #Output the two object
